b18ad004a0f6c39fe1fab1ab1ab0973fcac4b658
Sandesh Gnawali <sandeshgyawali25@gmail.com>
2023-08-04 14:30:10 -0500

    change in title and icon image

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
   return (
     <div>
         <h1 className='introduction'>Hi, It's me Sandesh Gnawali.</h1>
+        <a href='https://drive.google.com/file/d/1E6wI_r5-JtcFYPslzk8dZ0UsC_Y7CFqh/view?usp=drive_link' className='Resume'><h1>Resume</h1></a>
     </div>
   )
 }
