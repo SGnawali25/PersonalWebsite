f9e7a84d2e8c5917a4f8bc7f917f4b624cc938c5
Sandesh Gnawali <sandeshgyawali25@gmail.com>
2023-08-06 03:00:26 -0500

    change in bg-color and animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './App.css'
+import './index.css'
+import {Link} from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
+      <div className="intro-container">
         <h1 className='introduction'>Hi, It's me Sandesh Gnawali.</h1>
-        <a href='https://drive.google.com/file/d/1E6wI_r5-JtcFYPslzk8dZ0UsC_Y7CFqh/view?usp=drive_link' className='Resume'><h1>Resume</h1></a>
+      </div>
+      <a href='https://drive.google.com/file/d/1E6wI_r5-JtcFYPslzk8dZ0UsC_Y7CFqh/view?usp=drive_link' className='Resume'><h1>Resume</h1></a>
     </div>
   )
 }
