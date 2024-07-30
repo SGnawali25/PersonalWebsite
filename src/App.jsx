b4d480f57fa89738cb3829013a03a06815c00789
sgnawali25 <sandeshgyawali25@gmail.com>
2024-07-30 01:29:10 -0500

    changing url to new domain

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,19 @@
-import React from 'react'
-import './App.css'
-import './index.css'
-import {Link} from 'react-router-dom'
-import Header from './components/Header'
-import Home from './pages/Home'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Contact from './pages/Contact'
-import Skills from './pages/Skills'
-import Projects from './pages/Projects'
-import Experience from './pages/Experience'
-import Leadership from './pages/Leadership'
-import HeaderContent from './pages/HeaderContent'
+// import './App.css'
+// import './index.css'
+// import Header from './components/Header'
+// import Home from './pages/Home'
+// import { BrowserRouter as  Route, Routes } from 'react-router-dom'
+// import Contact from './pages/Contact'
+// import Skills from './pages/Skills'
 const App = () => {
   return (
     <div>
-      <Header/>
-      <Routes>
-        <Route path='/' Component={Home} exact />
-        <Route path='/contact' Component={Contact} exact/>
-        <Route path = '/Skills' Component={Skills} exact/>
-        <Route path='/projects' Component={Projects} exact/>
-        <Route path='/experience' Component={Experience} exact/>
-        <Route path='/leadership' Component={Leadership} exact/>
-        <Route path='/header' Component={HeaderContent} exact/>
-      </Routes>
+      <h1>Welcome to this page. Due to lazy loading I am currently working on this page.</h1>
+      <h2>Link to new Portfolio Website is below</h2>
+      <h3><a href='https://portfolio.sandeshgnawali.com.np'>Portfolio1</a></h3>
+
+      <h3> If the above link didn't work then follow this one</h3>
+      <h3><a href='https://sandeshportfolio.vercel.app'>Portfolio2</a></h3>
     </div>
   )
 }
